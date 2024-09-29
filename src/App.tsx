b1430ab1721d708aa2770c2b72be8b3bbacd6536
Amir97t <158777477+Amir97t@users.noreply.github.com>
2024-09-29 09:43:23 +0330

    show loading skeletons

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,26 @@ import { NavBar } from "./componenets/NavBar";
 import { GameGrid } from "./componenets/GameGrid";
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
+    <div>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
 
-      <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
-      </Show>
+        <Show above="lg">
+          <GridItem area="aside">Aside</GridItem>
+        </Show>
 
-      <GridItem area="main">
-        <GameGrid />
-      </GridItem>
-    </Grid>
+        <GridItem area="main">
+          <GameGrid />
+        </GridItem>
+      </Grid>
+    </div>
   );
 }
 
